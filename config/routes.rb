6b42d7ec7cb9240672c806cfb411d409ac56efd8
Vishay Bhatia <vishaybhatia@addictivegifts.com>
2016-08-30 13:37:31 +0400

    added spinner and ajax to stocks form

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
 
   get 'about' => 'pages#about'
 
+  get 'my_portfolio' => 'users#my_portfolio'
+
+  get 'search_stocks' => 'stocks#search'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
