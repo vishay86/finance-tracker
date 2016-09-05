cb368e864325e8dd9547f70b1c03bb35ca71f342
Vishay Bhatia <vishaybhatia@addictivegifts.com>
2016-09-05 09:37:19 +0400

    added new routes for js

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,22 @@ class UsersController < ApplicationController
   end
 
   def my_friends
+    @friendships = current_user.friends
   end
-  
+
+  def search
+    @users = User.search(params[:search_param])
+
+    if @users
+      @users = current_user.except_current_user(@users)
+      render partial: 'friends/lookup'
+    else
+      render status: :not_found, nothing: true
+    end
+  end
+
+  def add_friend
+  end
+
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {registrations: 'user/registrations'}
   resources :user_stocks, except: [:show, :edit, :update]
+  resources :users, only: [:show]
+  resources :friendships
 
   root 'pages#home'
 
@@ -12,6 +14,10 @@ Rails.application.routes.draw do
   get 'search_stocks' => 'stocks#search'
 
   get 'my_friends' => 'users#my_friends'
+
+  get 'search_friends' => 'users#search'
+
+  post 'add_friend' => 'users#add_friend'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
